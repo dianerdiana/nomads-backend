48444d0c058c0491f3bf57bb48c7baf362e3c210
dianerdiana9 <dianerdiana.de@gmail.com>
2022-10-30 09:35:40 +0700

    Replace render path home page

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,65 @@
+<!-- Navbar -->
+
+<div class="container">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white px-3 py-0">
+    <a class="navbar-brand" href="#">
+      <img src="{{ url('frontend/assets/icons/logo_nomads.png') }}" alt="navbar-brand" />
+    </a>
+    <button
+      class="navbar-toggler navbar-toggler-right"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navb"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+      data-target="#navb"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navb">
+      <ul class="navbar-nav ms-auto me-3">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Paket Travel</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Service
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider" /></li>
+            <li>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a href="#testimonialHeading" class="nav-link">Testimonial</a>
+        </li>
+      </ul>
+      <!-- Mobile Button -->
+      <form action="" class="form-inline d-sm-block d-md-none">
+        <button class="btn btn-login my-2 my-sm-0 px-4">Masuk</button>
+      </form>
+
+      <!-- Desktop Button  -->
+      <form action="" class="form-inline my-2 my-lg-0 d-none d-md-block">
+        <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4">
+          Masuk
+        </button>
+      </form>
+    </div>
+  </nav>
+</div>
---
+++ resources/views/includes/style.blade.php
@@ -0,0 +1,8 @@
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+  href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;500;600;700;800&display=swap"
+  rel="stylesheet"
+/>
+<link rel="stylesheet" href="{{ url("frontend/libraries/bootstrap/css/bootstrap.css") }}" />
+<link rel="stylesheet" href="{{ url("frontend/styles/main.css") }}" />
