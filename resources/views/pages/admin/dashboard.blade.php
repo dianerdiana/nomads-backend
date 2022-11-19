3afca67e085b2391dbdf4d93384a4e030391b30c
dianerdiana9 <dianerdiana.de@gmail.com>
2022-11-19 15:41:53 +0700

    Statistic Data

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -21,7 +21,7 @@
                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                   Paket Travel
                 </div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $travel_package }}</div>
               </div>
               <div class="col-auto">
                 <i class="fas fa-hotel fa-2x text-gray-300"></i>
@@ -40,7 +40,7 @@
                 <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                   Transaksi
                 </div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">14</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transaction }}</div>
               </div>
               <div class="col-auto">
                 <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -60,7 +60,7 @@
                   Pending
                 </div>
                 <div class="row no-gutters align-items-center">
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">8</div>
+                  <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transaction_pending }}</div>
                 </div>
               </div>
               <div class="col-auto">
@@ -80,7 +80,7 @@
                 <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
                   Sukses
                 </div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transaction_success }}</div>
               </div>
               <div class="col-auto">
                 <i class="fas fa-check fa-2x text-gray-300"></i>
