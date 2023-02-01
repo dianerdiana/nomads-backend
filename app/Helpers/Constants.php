0ac63684256a204b07ef46d5d62530c0cfaba02e
dianerdiana <dianerdiana.de@gmail.com>
2023-02-01 22:00:17 +0700

    Create helpers constants

---
+++ app/Helpers/Constants.php
@@ -0,0 +1,16 @@
+<?php
+
+class Constants{
+
+    const PAGINATE = 10;
+    const ADD_SUCCESS = "Data added successfully";
+    const UPDATE_SUCCESS = "Data updated successfully";
+    const DELETE_SUCCESS = "Data deleted successfully";
+    const DELETE_FAILED = "Data cannot be deleted";
+    const UPDATE_FAILED = "Failed updated data check request id";
+    const DATA_EMPTY = "No matching records found";
+    const REQUEST_EMPTY = "Request not found";
+    const SUCCESS = "success";
+    const FAILED = "failed";
+
+}
